edd9c442806d90930664254aef8fb748eb21620f
Andrew Wood <andrew.wood@ualberta.ca>
2020-07-11 13:56:49 -0600

    Adding in search functionality.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("random", views.random_page, name = "random"),
+    path("results", views.search, name = "search"),
     path("<str:entryTitle>", views.load_entry, name = "loadEntry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from markdown2 import markdown
 from django.shortcuts import redirect
 import random
+from django.db.models import Q
 
 from . import util
 
@@ -25,3 +26,22 @@ def random_page(request):
     entries = util.list_entries()
     selectedPage = random.choice(entries)
     return redirect(f'/{selectedPage}')
+
+#when they search, i want to submit that info to the results page
+#and then using that string, i loop over all entriese and check if search is a substring of entry
+#if it is, then add to list of results. then list all entries in results
+def search(request):
+    if request.method == "GET":
+        search_query = request.GET.get('search_box', None)
+        entries = util.list_entries()
+        results = []
+        # For all entries, check if my search query is a substring. If it is, append it to results.  
+        if search_query is not None:
+            for entry in entries:
+                if search_query.lower() in entry.lower():
+                    results.append(entry)
+            return render(request, "encyclopedia/results.html", {
+                "results" : results
+            })
+        else:
+            return render(request, "encyclopedia/results.html")
